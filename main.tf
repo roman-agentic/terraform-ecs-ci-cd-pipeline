b6d6b160044f5566cd5c87655c723a8821a10cae
rackman-campbell <leaddigit@gmail.com>
2019-12-24 02:15:14 +0000

    tf fixes after init problem

--- main.tf
+++ main.tf
@@ -1,5 +1,321 @@
-provider "aws" {
-  region = "${var.aws_region}"
+module "codepipeline_label" {
+  source     = "github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  enabled    = var.enabled
+  attributes = compact(concat(var.attributes, ["codepipeline"]))
+  delimiter  = var.delimiter
+  name       = var.name
+  namespace  = var.namespace
+  stage      = var.stage
+  tags       = var.tags
 }
 
-data "aws_caller_identity" "current" {}
+resource "aws_s3_bucket" "default" {
+  count         = var.enabled ? 1 : 0
+  bucket        = module.codepipeline_label.id
+  acl           = "private"
+  force_destroy = var.s3_bucket_force_destroy
+  tags          = module.codepipeline_label.tags
+}
+
+module "codepipeline_assume_role_label" {
+  source     = "github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  enabled    = var.enabled
+  attributes = compact(concat(var.attributes, ["codepipeline", "assume"]))
+  delimiter  = var.delimiter
+  name       = var.name
+  namespace  = var.namespace
+  stage      = var.stage
+  tags       = var.tags
+}
+
+resource "aws_iam_role" "default" {
+  count              = var.enabled ? 1 : 0
+  name               = module.codepipeline_assume_role_label.id
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    sid = ""
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  count      = var.enabled ? 1 : 0
+  role       = join("", aws_iam_role.default.*.id)
+  policy_arn = join("", aws_iam_policy.default.*.arn)
+}
+
+resource "aws_iam_policy" "default" {
+  count  = var.enabled ? 1 : 0
+  name   = module.codepipeline_label.id
+  policy = data.aws_iam_policy_document.default.json
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid = ""
+
+    actions = [
+      "ec2:*",
+      "elasticloadbalancing:*",
+      "autoscaling:*",
+      "cloudwatch:*",
+      "s3:*",
+      "sns:*",
+      "cloudformation:*",
+      "rds:*",
+      "sqs:*",
+      "ecs:*",
+      "iam:PassRole"
+    ]
+
+    resources = ["*"]
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  count      = var.enabled ? 1 : 0
+  role       = join("", aws_iam_role.default.*.id)
+  policy_arn = join("", aws_iam_policy.s3.*.arn)
+}
+
+module "codepipeline_s3_policy_label" {
+  source     = "github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  enabled    = var.enabled
+  attributes = compact(concat(var.attributes, ["codepipeline", "s3"]))
+  delimiter  = var.delimiter
+  name       = var.name
+  namespace  = var.namespace
+  stage      = var.stage
+  tags       = var.tags
+}
+
+resource "aws_iam_policy" "s3" {
+  count  = var.enabled ? 1 : 0
+  name   = module.codepipeline_s3_policy_label.id
+  policy = join("", data.aws_iam_policy_document.s3.*.json)
+}
+
+data "aws_iam_policy_document" "s3" {
+  count = var.enabled ? 1 : 0
+
+  statement {
+    sid = ""
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject"
+    ]
+
+    resources = [
+      join("", aws_s3_bucket.default.*.arn),
+      "${join("", aws_s3_bucket.default.*.arn)}/*"
+    ]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  count      = var.enabled ? 1 : 0
+  role       = join("", aws_iam_role.default.*.id)
+  policy_arn = join("", aws_iam_policy.codebuild.*.arn)
+}
+
+module "codebuild_label" {
+  source     = "github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  enabled    = var.enabled
+  attributes = compact(concat(var.attributes, ["codebuild"]))
+  delimiter  = var.delimiter
+  name       = var.name
+  namespace  = var.namespace
+  stage      = var.stage
+  tags       = var.tags
+}
+
+resource "aws_iam_policy" "codebuild" {
+  count  = var.enabled ? 1 : 0
+  name   = module.codebuild_label.id
+  policy = data.aws_iam_policy_document.codebuild.json
+}
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    sid = ""
+
+    actions = [
+      "codebuild:*"
+    ]
+
+    resources = [module.codebuild.project_id]
+    effect    = "Allow"
+  }
+}
+
+data "aws_caller_identity" "default" {
+}
+
+data "aws_region" "default" {
+}
+
+module "codebuild" {
+  source                = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=tags/0.17.0"
+  enabled               = var.enabled
+  namespace             = var.namespace
+  name                  = var.name
+  stage                 = var.stage
+  build_image           = var.build_image
+  build_compute_type    = var.build_compute_type
+  build_timeout         = var.build_timeout
+  buildspec             = var.buildspec
+  delimiter             = var.delimiter
+  attributes            = concat(var.attributes, ["build"])
+  tags                  = var.tags
+  privileged_mode       = var.privileged_mode
+  aws_region            = var.region != "" ? var.region : data.aws_region.default.name
+  aws_account_id        = var.aws_account_id != "" ? var.aws_account_id : data.aws_caller_identity.default.account_id
+  image_repo_name       = var.image_repo_name
+  image_tag             = var.image_tag
+  github_token          = var.github_oauth_token
+  environment_variables = var.environment_variables
+  badge_enabled         = var.badge_enabled
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_s3" {
+  count      = var.enabled ? 1 : 0
+  role       = module.codebuild.role_id
+  policy_arn = join("", aws_iam_policy.s3.*.arn)
+}
+
+resource "aws_codepipeline" "default" {
+  count    = var.enabled ? 1 : 0
+  name     = module.codepipeline_label.id
+  role_arn = join("", aws_iam_role.default.*.arn)
+
+  artifact_store {
+    location = join("", aws_s3_bucket.default.*.bucket)
+    type     = "S3"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.default,
+    aws_iam_role_policy_attachment.s3,
+    aws_iam_role_policy_attachment.codebuild,
+    aws_iam_role_policy_attachment.codebuild_s3
+  ]
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["code"]
+
+      configuration = {
+        OAuthToken           = var.github_oauth_token
+        Owner                = var.repo_owner
+        Repo                 = var.repo_name
+        Branch               = var.branch
+        PollForSourceChanges = var.poll_source_changes
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name     = "Build"
+      category = "Build"
+      owner    = "AWS"
+      provider = "CodeBuild"
+      version  = "1"
+
+      input_artifacts  = ["code"]
+      output_artifacts = ["task"]
+
+      configuration = {
+        ProjectName = module.codebuild.project_name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ECS"
+      input_artifacts = ["task"]
+      version         = "1"
+
+      configuration = {
+        ClusterName = var.ecs_cluster_name
+        ServiceName = var.service_name
+      }
+    }
+  }
+}
+
+resource "random_string" "webhook_secret" {
+  count  = var.enabled && var.webhook_enabled ? 1 : 0
+  length = 32
+
+  # Special characters are not allowed in webhook secret (AWS silently ignores webhook callbacks)
+  special = false
+}
+
+locals {
+  webhook_secret = join("", random_string.webhook_secret.*.result)
+  webhook_url    = join("", aws_codepipeline_webhook.webhook.*.url)
+}
+
+resource "aws_codepipeline_webhook" "webhook" {
+  count           = var.enabled && var.webhook_enabled ? 1 : 0
+  name            = module.codepipeline_label.id
+  authentication  = var.webhook_authentication
+  target_action   = var.webhook_target_action
+  target_pipeline = join("", aws_codepipeline.default.*.name)
+
+  authentication_configuration {
+    secret_token = local.webhook_secret
+  }
+
+  filter {
+    json_path    = var.webhook_filter_json_path
+    match_equals = var.webhook_filter_match_equals
+  }
+}
+
+module "github_webhooks" {
+  source               = "git::https://github.com/cloudposse/terraform-github-repository-webhooks.git?ref=tags/0.5.0"
+  enabled              = var.enabled && var.webhook_enabled ? true : false
+  github_organization  = var.repo_owner
+  github_repositories  = [var.repo_name]
+  github_token         = var.github_webhooks_token
+  webhook_url          = local.webhook_url
+  webhook_secret       = local.webhook_secret
+  webhook_content_type = "json"
+  events               = var.github_webhook_events
+}
--- variables.tf
+++ variables.tf
@@ -1,99 +1,195 @@
-################################
-###### QUICK EDIT'S HERE  ######
-################################
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
 
-###### CLUSTER OPTIONS  ######
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
+}
 
-# Customize the Cluster Name
-variable "cluster_name" {
+variable "name" {
+  type        = string
+  description = "Name of the application"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  description = "Additional attributes (_e.g._ \"1\")"
+  default     = []
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Additional tags (_e.g._ { BusinessUnit : ABC })"
+  default     = {}
+}
+
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Enable `CodePipeline` creation"
+}
+
+variable "ecs_cluster_name" {
+  type        = string
   description = "ECS Cluster Name"
-  default     = "web-app"
 }
 
-# Customize your ECR Registry Name
-variable "app_repository_name" {
-  description = "ECR Repository Name"
-  default     = "web-app"
+variable "service_name" {
+  type        = string
+  description = "ECS Service Name"
 }
 
-###### APPLICATION OPTIONS  ######
-variable "container_name" {
-  description = "Container app name"
-  default     = "micro-api"
+variable "github_oauth_token" {
+  type        = string
+  description = "GitHub OAuth Token with permissions to access private repositories"
 }
 
-# Number of containers
-variable "desired_tasks" {
-  description = "Number of containers desired to run app task"
-  default     = 2
+variable "github_webhooks_token" {
+  type        = string
+  default     = ""
+  description = "GitHub OAuth Token with permissions to create webhooks. If not provided, can be sourced from the `GITHUB_TOKEN` environment variable"
 }
 
-variable "min_tasks" {
-  description = "Minimum"
-  default     = 2
+variable "github_webhook_events" {
+  type        = list(string)
+  description = "A list of events which should trigger the webhook. See a list of [available events](https://developer.github.com/v3/activity/events/types/)"
+  default     = ["push"]
 }
 
-variable "max_tasks" {
-  description = "Maximum"
-  default     = 4
+variable "repo_owner" {
+  type        = string
+  description = "GitHub Organization or Username"
 }
 
-variable "cpu_to_scale_up" {
-  description = "CPU % to Scale Up the number of containers"
-  default     = 80
+variable "repo_name" {
+  type        = string
+  description = "GitHub repository name of the application to be built and deployed to ECS"
 }
 
-variable "cpu_to_scale_down" {
-  description = "CPU % to Scale Down the number of containers"
-  default     = 30
+variable "branch" {
+  type        = string
+  description = "Branch of the GitHub repository, _e.g._ `master`"
 }
 
-# Desired CPU
-variable "desired_task_cpu" {
-  description = "Desired CPU to run your tasks"
-  default     = "256"
+variable "badge_enabled" {
+  type        = bool
+  default     = false
+  description = "Generates a publicly-accessible URL for the projects build badge. Available as badge_url attribute when enabled"
 }
 
-# Desired Memory
-variable "desired_task_memory" {
-  description = "Desired memory to run your tasks"
-  default     = "512"
+variable "build_image" {
+  type        = string
+  default     = "aws/codebuild/docker:17.09.0"
+  description = "Docker image for build environment, _e.g._ `aws/codebuild/docker:docker:17.09.0`"
 }
 
-# Listener Application Load Balancer Port
-variable "alb_port" {
-  description = "Origin Application Load Balancer Port"
-  default     = "80"
+variable "build_compute_type" {
+  type        = string
+  default     = "BUILD_GENERAL1_SMALL"
+  description = "`CodeBuild` instance size. Possible values are: `BUILD_GENERAL1_SMALL` `BUILD_GENERAL1_MEDIUM` `BUILD_GENERAL1_LARGE`"
 }
 
-# Target Group Load Balancer Port
-variable "container_port" {
-  description = "Destination Application Load Balancer Port"
-  default     = "80"
+variable "build_timeout" {
+  type        = number
+  default     = 60
+  description = "How long in minutes, from 5 to 480 (8 hours), for AWS CodeBuild to wait until timing out any related build that does not get marked as completed"
 }
 
-###### GITHUB OPTIONS  ######
-
-# Github Repository Owner
-variable "git_repository_owner" {
-  description = "Github Repository Owner"
-  default     = "msfidelis"
+variable "buildspec" {
+  type        = string
+  default     = ""
+  description = "Declaration to use for building the project. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html)"
 }
 
-# Github Repository Project Name
-variable "git_repository_name" {
-  description = "Project name on Github"
-  default     = "micro-api"
+# https://www.terraform.io/docs/configuration/variables.html
+# It is recommended you avoid using boolean values and use explicit strings
+variable "poll_source_changes" {
+  type        = bool
+  default     = false
+  description = "Periodically check the location of your source content and run the pipeline if changes are detected"
 }
 
-# Default Branch
-variable "git_repository_branch" {
-  description = "Github Project Branch"
-  default     = "master"
+variable "privileged_mode" {
+  type        = bool
+  default     = false
+  description = "If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
 }
 
-# Customize your AWS Region
-variable "aws_region" {
-  description = "AWS Region for the VPC"
-  default     = "us-east-1"
+variable "region" {
+  type        = string
+  description = "AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "aws_account_id" {
+  type        = string
+  default     = ""
+  description = "AWS Account ID. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "image_repo_name" {
+  type        = string
+  description = "ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "image_tag" {
+  type        = string
+  default     = "latest"
+  description = "Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "environment_variables" {
+  type = list(object(
+    {
+      name  = string
+      value = string
+  }))
+
+  default     = []
+  description = "A list of maps, that contain both the key 'name' and the key 'value' to be used as additional environment variables for the build"
+}
+
+variable "webhook_enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating any webhook resources"
+  default     = true
+}
+
+variable "webhook_target_action" {
+  type        = string
+  description = "The name of the action in a pipeline you want to connect to the webhook. The action must be from the source (first) stage of the pipeline"
+  default     = "Source"
+}
+
+variable "webhook_authentication" {
+  type        = string
+  description = "The type of authentication to use. One of IP, GITHUB_HMAC, or UNAUTHENTICATED"
+  default     = "GITHUB_HMAC"
+}
+
+variable "webhook_filter_json_path" {
+  type        = string
+  description = "The JSON path to filter on"
+  default     = "$.ref"
+}
+
+variable "webhook_filter_match_equals" {
+  type        = string
+  description = "The value to match on (e.g. refs/heads/{Branch})"
+  default     = "refs/heads/{Branch}"
+}
+
+variable "s3_bucket_force_destroy" {
+  type        = bool
+  description = "A boolean that indicates all objects should be deleted from the CodePipeline artifact store S3 bucket so that the bucket can be destroyed without error"
+  default     = false
 }
